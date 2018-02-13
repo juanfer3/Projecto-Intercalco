ad0b421795f1a36a3b9886bd3f0d73906636dcca
juanfer3 <jfvilladiego2@misena.edu.co>
2018-02-13 14:19:58 -0500

    Commit Respaldo antes de crear materiales

--- app/models/orden_produccion.rb
+++ app/models/orden_produccion.rb
@@ -6,14 +6,9 @@ class OrdenProduccion < ApplicationRecord
   has_many :compromisos_de_entrega, inverse_of: :orden_produccion, dependent: :destroy
   accepts_nested_attributes_for :compromisos_de_entrega, reject_if: :all_blank, allow_destroy: true
 
-  has_many :formulas_tinta, inverse_of: :tinta_formulada, dependent: :destroy
-  accepts_nested_attributes_for :formulas_tinta, reject_if: :all_blank, allow_destroy: true
 
-  has_many :transiciones, inverse_of: :tinta_formulada, dependent: :destroy
-  accepts_nested_attributes_for :transiciones, reject_if: :all_blank, allow_destroy: true
 
-  has_many :facturas, inverse_of: :orden_produccion, dependent: :destroy
-  accepts_nested_attributes_for :facturas, reject_if: :all_blank, allow_destroy: true
+
 
   has_many :contenedores_de_ordenes, inverse_of: :orden_produccion, dependent: :destroy
   accepts_nested_attributes_for :contenedores_de_ordenes, reject_if: :all_blank, allow_destroy: true
