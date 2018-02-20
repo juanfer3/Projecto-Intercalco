d4556ee24e6816138f3647c9792cac5d6df879e2
juanfer3 <jfvilladiego2@misena.edu.co>
2018-02-20 14:02:59 -0500

    Corrigiendo devise4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { :host => 'intercalco.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://intercalco.herokuapp.com/' }
 end
