c34646a1a69b1e52dad672b578d2ae3b55bd573f
juanfer3 <jfvilladiego2@misena.edu.co>
2018-02-21 10:47:48 -0500

    Montaje from excel

--- app/models/montaje.rb
+++ app/models/montaje.rb
@@ -155,12 +155,12 @@ class Montaje < ApplicationRecord
 
     (2..spreadsheet.last_row).each do |i|
       puts "*******************Entra Al excel*********************"
-      codigo_de_p = spreadsheet.row(i)[3].to_s.upcase
-      codigo_de_pieza= codigo_de_p
+      codigo_de_pieza = spreadsheet.row(i)[3]
+
 
       puts "*******************cod pieza #{codigo_de_pieza}*********************"
       if codigo_de_pieza != ""
-        puts "*******************La Pieza NO es un String vacio y la pieza es "+codigo_de_pieza.to_s+"*********************"
+        puts "*******************La Pieza NO es un String vacio y la pieza es *********************"
 
             @busq_pieza = Pieza.find_by(codigo: codigo_de_pieza)
             puts "*******************Consulto La Pieza y es *********************"
@@ -199,58 +199,94 @@ class Montaje < ApplicationRecord
                                                     puts "****************************************El Cliente -Si- Existe y su id es: #{@cli.id}**********************"
                                                     @cliente_id = @cli.id
 
-                                                    @nombre_user = spreadsheet.row(i)[5]
-                                                    @users = User.find_by(nombre: @nombre_user)
-                                                    puts "****************************************El Vendedor se Busco**********************"
-
-
-                                                        @user_id=@users.id
-                                                        puts "****************************************El User -Si- Existe y su id es: #{@users.id}**********************"
 
                                                   end
 
 
 
 
-                                                              @linea_produccion_nombre = spreadsheet.row(i)[6]
-                                                              puts "=============Esta es la LInea==#{@linea_produccion_nombre}==============="
+                                                              @linea_produccion_nombre = spreadsheet.row(i)[7].to_s.upcase
+                                                              puts "=============Esta es la Linea==#{@linea_produccion_nombre}==============="
 
                                                               @lineas_produccion = LineaProducto.find_by(nombre: @linea_produccion_nombre)
-                                                              puts "===============se busco linea de producto y es #{@lineas_produccion.id}==============="
+                                                              puts "===============se busco linea de producto y es #{@lineas_produccion}==============="
 
-
-                                                                  @linea_produccion_id=@lineas_produccion.id
-
-
-
-
-
-
-                                                              @maquinas_nombre = spreadsheet.row(i)[8]
-                                                              @maquinas = Maquina.all
-
-                                                              @maquinas.each do |maquina|
-                                                                if maquina.nombre == @maquinas_nombre
-                                                                  @maquina_id=maquina.id
-                                                                  puts "===============maquina id: "+@maquina_id.to_s+"==============="
+                                                              if @lineas_produccion != nil
+                                                                puts "********************La linea existe********************"
+                                                                @linea_produccion_id=@lineas_produccion.id
+                                                              else
+                                                                puts "********************La linea no existe********************"
+                                                                linea = LineaProducto.new(nombre: @linea_produccion_nombre)
+                                                                if linea.save
+                                                                  puts "******************Se creo la linea produccion**********************"
+                                                                  @linea_produccion_id=linea.id
                                                                 end
+
                                                               end
 
 
-                                                              @linea_de_color_nombre = spreadsheet.row(i)[9]
 
-                                                              @lineas_de_colores = LineaDeColor.all
 
-                                                              @lineas_de_colores.each do |linea_de_color|
-                                                                if linea_de_color.nombre == @linea_de_color_nombre
-                                                                  @linea_de_color_id=linea_de_color.id
-                                                                  puts "===============color id:"+@linea_de_color_id.to_s+"==============="
+
+
+
+
+                                                              @maquinas_nombre = spreadsheet.row(i)[8].to_s.upcase
+                                                              @maquinas = Maquina.find_by(nombre: @maquinas_nombre)
+
+                                                              if @maquinas != nil
+                                                                puts "********************La maquina existe********************"
+                                                                @maquina_id = @maquinas.id
+                                                              else
+                                                                puts "********************La maquina no existe********************"
+                                                                maquina = Maquina.new(nombre: @maquinas_nombre)
+                                                                if maquina.save
+                                                                  puts "******************Se creo la maquina produccion**********************"
+                                                                  @maquina_id = maquina.id
                                                                 end
+
                                                               end
+
+
+
+
+
+
+
+                                                              @linea_de_color_nombre = spreadsheet.row(i)[9].to_s.upcase
+
+                                                              @lineas_de_colores = LineaDeColor.find_by(nombre: @linea_de_color_nombre)
+                                                        
+
+                                                              if @lineas_de_colores != nil
+                                                                puts "********************Los colores existe********************"
+                                                                @linea_de_color_id=@lineas_de_colores.id
+                                                              else
+                                                                puts "********************Los colores no existe********************"
+                                                                color = LineaDeColor.new(nombre: @linea_de_color_nombre)
+                                                                if color.save
+                                                                  puts "******************Se creo el color produccion**********************"
+                                                                  @linea_de_color_id=color.id
+                                                                end
+
+                                                              end
+
+
+                                                            material_nombre = spreadsheet.row(i)[8].to_s.upcase
+                                                            puts "****************buscando materiales************************"
+                                                            material = Material.find_by(descripcion:material_nombre)
+
+                                                            if material == nil
+                                                              material_save = Material.new(codigo:"POR DEFINIR",descripcion:material_nombre)
+                                                              @material_id=material_save.id
+                                                            else
+                                                              @material_id=material.id
+                                                            end
 
                                                             codigo_mo=spreadsheet.row(i)[1].to_s.upcase
                                                             nombre_mo=spreadsheet.row(i)[2].to_s.upcase
-                                                            formato_mo = Montaje.new( codigo:codigo_mo, nombre: nombre_mo, cliente_id: @cliente_id, maquina_id: @maquina_id, linea_de_color_id: @linea_de_color_id, linea_producto_id:@linea_produccion_id)
+                                                            formato_mo = Montaje.new( codigo:codigo_mo, nombre: nombre_mo, cliente_id: @cliente_id, maquina_id: @maquina_id, linea_de_color_id: @linea_de_color_id, linea_producto_id:@linea_produccion_id, material_id: @material_id)
+
                                                             if formato_mo.save
                                                               puts "****************************************Montaje Creado**********************"
                                                               codigo_pieza = spreadsheet.row(i)[3].to_s.upcase
