c4a1cdcb540beb220771aef26c9f6388c3965989
juanfer3 <jfvilladiego2@misena.edu.co>
2018-03-12 14:25:55 -0500

    Ordenes produccion

--- app/controllers/ordenes_produccion_controller.rb
+++ app/controllers/ordenes_produccion_controller.rb
@@ -4,7 +4,7 @@ class OrdenesProduccionController < ApplicationController
   # GET /ordenes_produccion
   # GET /ordenes_produccion.json
   def index
-    @ordenes_produccion = OrdenProduccion.joins(:compromisos_de_entrega).paginate(page: params[:page], per_page: 20).order('compromisos_de_entrega.fecha_de_compromiso DESC').distinct
+    @ordenes_produccion = OrdenProduccion.all.paginate(page: params[:page], per_page: 20).order('numero_de_orden DESC').distinct
     respond_to do |format|
       format.html
       format.js
