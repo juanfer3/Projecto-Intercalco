f2863b7de5b26371ba056fe2eb1e2634eb45bde7
juanfer3 <jfvilladiego2@misena.edu.co>
2018-04-23 14:20:52 -0500

    EXPORTAR A EXCEL

--- app/models/maquina.rb
+++ app/models/maquina.rb
@@ -38,7 +38,7 @@ class Maquina < ApplicationRecord
 
       ordenes_produccion.each do |orden|
 
-        puts"====esta son las ordenes -#{orden["clientes"]["nombre"]}-==="
+        puts"====esta son las ordenes -#{orden["numero_de_orden"]}-==="
       end
 
 
