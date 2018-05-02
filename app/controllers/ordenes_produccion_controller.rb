d71a8840e3e9ecd4b7a032966f300181759b879a
juanfer3 <jfvilladiego2@misena.edu.co>
2018-05-02 11:27:55 -0500

    GENERAR INFORME DE OPORTNIDAD

--- app/controllers/ordenes_produccion_controller.rb
+++ app/controllers/ordenes_produccion_controller.rb
@@ -52,9 +52,19 @@ class OrdenesProduccionController < ApplicationController
   maquina_efi = "DIGITAL EFI"
   maquina_mutoh = "DIGITAL MUTOH"
 
-  @ordenes_produccion = OrdenProduccion.joins(:compromisos_de_entrega, :montaje => [:linea_producto, :contenedores_de_maquinas]).paginate(page: params[:page], per_page: 20).where("compromisos_de_entrega.fecha_de_compromiso >= ? AND ordenes_produccion.entregado = ? AND (contenedores_de_maquinas.maquina.nombre = ? OR contenedores_de_maquinas.maquina.nombre = ?) ", hoy, entregado, maquina_efi, maquina_mutoh).order("compromisos_de_entrega.fecha_de_compromiso")
-  todas_las_ordenes = OrdenProduccion.joins(:montaje =>[:contenedores_de_maquinas=>[:maquina]]).where("ordenes_produccion.entregado = ? AND (contenedores_de_maquinas.maquina.nombre = ? OR contenedores_de_maquinas.maquina.nombre = ? AND (contenedores_de_maquinas.maquina.nombre = ? OR contenedores_de_maquinas.maquina.nombre = ?))", entregado, maquina_efi, maquina_mutoh).order("ordenes_produccion.numero_de_orden")
-  @ordenes_prioridad = OrdenProduccion.joins(:compromisos_de_entrega, :montaje =>[:contenedores_de_maquinas=>[:maquina]]).where("compromisos_de_entrega.fecha_de_compromiso < ? AND   ordenes_produccion.entregado = ? AND (contenedores_de_maquinas.maquina.nombre = ? OR contenedores_de_maquinas.maquina.nombre = ?)", hoy,entregado, maquina_efi, maquina_mutoh).order("compromisos_de_entrega.fecha_de_compromiso")
+  @ordenes_produccion = OrdenProduccion.joins(:compromisos_de_entrega, :montaje => [:linea_producto, :contenedores_de_maquinas])
+        .paginate(page: params[:page], per_page: 20)
+        .where("compromisos_de_entrega.fecha_de_compromiso >= ? AND ordenes_produccion.entregado = ? AND (contenedores_de_maquinas.maquina.nombre = ? OR contenedores_de_maquinas.maquina.nombre = ?) ", hoy, entregado, maquina_efi, maquina_mutoh)
+        .order("compromisos_de_entrega.fecha_de_compromiso")
+
+  todas_las_ordenes = OrdenProduccion.joins(:montaje =>[:contenedores_de_maquinas=>[:maquina]])
+        .where("ordenes_produccion.entregado = ? AND (contenedores_de_maquinas.maquina.nombre = ? OR contenedores_de_maquinas.maquina.nombre = ?)", entregado, maquina_efi, maquina_mutoh)
+        .order("ordenes_produccion.numero_de_orden")
+
+  @ordenes_prioridad = OrdenProduccion.joins(:compromisos_de_entrega, :montaje =>[:contenedores_de_maquinas=>[:maquina]])
+        .where("compromisos_de_entrega.fecha_de_compromiso < ? AND   ordenes_produccion.entregado = ? ", hoy,entregado)
+        .order("compromisos_de_entrega.fecha_de_compromiso")
+
    @ordenes_sin_fecha = []
    #iteracion 1
    todas_las_ordenes = todas_las_ordenes.sort_by{|a| a.compromisos_de_entrega.sort_by{|b| b["fecha_de_compromiso"].to_s.split('/') } }
