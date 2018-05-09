3dc0065c54e5a8732de083b273c655067f92728b
juanfer3 <jfvilladiego2@misena.edu.co>
2018-05-09 14:01:57 -0500

    UPDATE ROLES AND MAQUINAS ADMIN

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -105,7 +105,7 @@ end
 
     respond_to do |format|
       if @cliente.save
-        format.html { redirect_to @cliente, notice: 'Cliente was successfully created.' }
+        format.html { redirect_to clientes_url, notice: 'Cliente was successfully created.' }
         format.json { render :show, status: :created, location: @cliente }
       else
         format.html { render :new }
@@ -119,7 +119,7 @@ end
   def update
     respond_to do |format|
       if @cliente.update(cliente_params)
-        format.html { redirect_to @cliente, notice: 'Cliente was successfully updated.' }
+        format.html { redirect_to clientes_url, notice: 'Cliente was successfully updated.' }
         format.json { render :show, status: :ok, location: @cliente }
       else
         format.html { render :edit }
